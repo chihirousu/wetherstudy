4d51c2ae426f4cbe9bb8fa4416c0d19f7000c1c8
chihiro Uejo <kooy913@gmail.com>
2023-10-09 15:27:02 +0900

    遷移先画面の調整

--- app/src/main/java/com/websarva/wings/android/asyncsample/ResultActivity.java
+++ app/src/main/java/com/websarva/wings/android/asyncsample/ResultActivity.java
@@ -3,6 +3,7 @@ package com.websarva.wings.android.asyncsample;
 import androidx.annotation.UiThread;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
@@ -14,14 +15,17 @@ public class ResultActivity extends AppCompatActivity {
     TextView tvWeatherTelop;
     TextView tvWeatherDesc;
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
+        Intent intent = getIntent();
+
+
+        Log.i("AsyncSample","Result onCreate() called.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-
         //天気を表示するtextビューを取得
         tvWeatherTelop = findViewById(R.id.tvWeatherTelop);
         tvWeatherDesc = findViewById(R.id.tvWeatherDesc);
@@ -29,5 +33,44 @@ public class ResultActivity extends AppCompatActivity {
         //tvWeatherTelop.setText(telop);
         //tvWeatherDesc.setText(desc);
 
+
+
     }
+    @Override
+    public void onStart(){
+        Log.i("AsyncSample","Result onCreate() called.");
+        super.onStart();
+    }
+
+    @Override
+    public void onRestart(){
+        Log.i("AsyncSample","Result onCreate() called.");
+        super.onRestart();
+    }
+
+    @Override
+    public void onResume(){
+        Log.i("AsyncSample","Result onCreate() called.");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause(){
+        Log.i("AsyncSample","Result onCreate() called.");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop(){
+        Log.i("AsyncSample","Result onCreate() called.");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy(){
+        Log.i("AsyncSample","Result onCreate() called.");
+        super.onDestroy();
+    }
+
+
 }
